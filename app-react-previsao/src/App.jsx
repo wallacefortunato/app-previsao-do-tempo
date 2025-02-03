24b2b2d0b256b7bacfcbfdc87ae88594f9319464
wallacefortunato <wallacetfortunato@gmail.com>
2025-02-03 13:25:47 -0300

    feat: installing axios

--- app-react-previsao/src/App.jsx
+++ app-react-previsao/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const inputRef = useRef(null)
 
+  function searchCity() {
+    console.log(inputRef.current.value)
+  }
   return (
     <div>
       <h1>Previsão do tempo</h1>
-      <input type="text" placeholder='Digite o nome da cidade'/>
-      <button>Buscar</button>
+      <input ref={inputRef} type="text" placeholder='Digite o nome da cidade'/>
+      <button onClick={searchCity}>Buscar</button>
     </div>
   )
 }
